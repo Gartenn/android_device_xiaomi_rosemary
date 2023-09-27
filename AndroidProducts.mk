2862aea39e6d48f05834b652177663fa1158808f
Garten <brilliangarten@gmail.com>
2023-09-27 12:52:09 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/voltage_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_rosemary-user \
-    voltage_rosemary-userdebug \
-    voltage_rosemary-eng
+    lineage_rosemary-user \
+    lineage_rosemary-userdebug \
+    lineage_rosemary-eng
