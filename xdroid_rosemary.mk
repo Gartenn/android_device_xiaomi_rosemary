3b8e072476fb005f8fce9f92ba575d97ca763d8e
Garten <brilliangarten@gmail.com>
2023-10-02 08:25:20 +0700

    Update and rename droidx_rosemary.mk to xdroid_rosemary.mk

--- xdroid_rosemary.mk
+++ xdroid_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := droidx_rosemary
+PRODUCT_NAME := xdroid_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
