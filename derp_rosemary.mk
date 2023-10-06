673726939f4e5ef31af87defeae7bdcf33436f68
Garten <brilliangarten@gmail.com>
2023-10-06 15:18:04 +0700

    Update and rename superior_rosemary.mk to derp_rosemary.mk

--- derp_rosemary.mk
+++ derp_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/derp/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := superior_rosemary
+PRODUCT_NAME := derp_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -30,11 +30,4 @@ BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWX
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
-USE_MOTO_CALCULATOR := true
-USE_MOTO_CLOCK := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
 BUILD_WITH_GAPPS := true
-USE_QUICKPIC := true
-USE_DUCKDUCKGO := false
