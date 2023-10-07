b7e162842747b22ec7b9de8710a04ff657cfb335
Garten <brilliangarten@gmail.com>
2023-10-07 11:05:56 +0700

    Update and rename evolution_rosemary.mk to derp_rosemary.mk

--- derp_rosemary.mk
+++ derp_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/ciphe/config/common.mk)
+$(call inherit-product, vendor/derp/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := cipher_rosemary
+PRODUCT_NAME := derp_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
