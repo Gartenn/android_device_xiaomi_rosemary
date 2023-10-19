f1109be25fed59ad87720ad6d5341fecc37f0cc3
Garten <brilliangarten@gmail.com>
2023-10-19 16:23:26 +0700

    Update and rename derp_rosemary.mk to lineage_rosemary.mk

--- lineage_rosemary.mk
+++ lineage_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/derp/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := derp_rosemary
+PRODUCT_NAME := lineage_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -26,3 +26,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V13.0.5.0.SFFTWXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWXM:user/release-keys
+
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := unofficial
+ALPHA_MAINTAINER := macropyre
