c8992e38e8346fa6878e59327732a552a621eb01
Garten <brilliangarten@gmail.com>
2023-10-21 09:12:51 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosemary.mk
+    $(LOCAL_DIR)/havoc_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosemary-user \
-    lineage_rosemary-userdebug \
-    lineage_rosemary-eng
+    havoc_rosemary-user \
+    havoc_rosemary-userdebug \
+    havoc_rosemary-eng
