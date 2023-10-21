a472fb4a9cdc69649f777cbf10f542fef0862117
Garten <brilliangarten@gmail.com>
2023-10-21 09:14:11 +0700

    Update and rename lineage_rosemary.mk to havoc_rosemary.mk

--- havoc_rosemary.mk
+++ havoc_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := lineage_rosemary
+PRODUCT_NAME := havoc_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -28,11 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWXM:user/release-keys
 
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_ENABLE_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_GMAIL := true
-TARGET_INCLUDE_GOOGLE_MAPS := true
-
-# Maintainer
-ALPHA_BUILD_TYPE := unofficial
-ALPHA_MAINTAINER := macropyre
