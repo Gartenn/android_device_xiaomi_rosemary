32b171cbdc00d12d6d3a91047dcfa33ffb6170b9
Garten <brilliangarten@gmail.com>
2023-10-28 15:29:21 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosemary.mk
+    $(LOCAL_DIR)/banana_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosemary-user \
-    lineage_rosemary-userdebug \
-    lineage_rosemary-eng
+    banana_rosemary-user \
+    banana_rosemary-userdebug \
+    banana_rosemary-eng
