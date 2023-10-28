de78985a616e5af4ae4b68367edb03d43515c77f
Garten <brilliangarten@gmail.com>
2023-10-28 13:42:56 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/tequila_rosemary.mk
+    $(LOCAL_DIR)/lineage_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    tequila_rosemary-user \
-    tequila_rosemary-userdebug \
-    tequila_rosemary-eng
+    lineage_rosemary-user \
+    lineage_rosemary-userdebug \
+    lineage_rosemary-eng
