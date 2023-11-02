fe0d9f27963ec99f28e97f5e885f74727300dd6b
Garten <brilliangarten@gmail.com>
2023-11-02 08:30:50 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_rosemary.mk
+    $(LOCAL_DIR)/afterlife_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_rosemary-user \
-    cherish_rosemary-userdebug \
-    cherish_rosemary-eng
+    afterlife_rosemary-user \
+    afterlife_rosemary-userdebug \
+    afterlife_rosemary-eng
