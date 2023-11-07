7d51d6fc0b3fae8ff15c5adc0d3fc2e88050ff86
Garten <brilliangarten@gmail.com>
2023-11-07 19:12:04 +0700

    Update and rename droidx_rosemary.mk to spark_rosemary.mk

--- spark_rosemary.mk
+++ spark_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := droidx_rosemary
+PRODUCT_NAME := spark_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -27,8 +27,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWXM:user/release-keys
 
-TARGET_SUPPORTS_QUICK_TAP := true
-DROIDX_GAPPS := true
-TARGET_CORE_GMS := true
-TARGET_SUPPORTS_BLUR := true
+TARGET_USES_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
